4ec7c8b5a4d74c0a785ad3105703212ea126dd1b
Mohammad <zeenfaiz@gmail.com>
2017-07-25 11:50:00 +0400

    added light project

--- light/ViewController.swift
+++ light/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var lightOn = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateUI()
     }
 
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn = !lightOn
+        updateUI()
+    }
+    
+    func updateUI(){
+        if(lightOn){
+            // .white and .black enums are used from UIColor which
+            // is already import from UIKit
+            view.backgroundColor = .white
+        }
+        else{
+            view.backgroundColor = .black
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
